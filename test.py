ae4e35634bf0f5fd567fa2b65ff492510d39d297
Jouni Seppänen <jouni.seppanen@reaktor.com>
2020-08-07 07:48:57 +0300

    Fix

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import io
-import urllib
+from urllib import request
 
 def test_fetching():
     url = 'https://matplotlib.org/1.5.0/_static/logo2.png'
-    with urllib.request.urlopen(parsed) as response:
+    with request.urlopen(url) as response:
         response = io.BytesIO(response.read())
-        assert len(response) > 0
+        assert len(response.getbuffer()) == 19496
