688ee6311bd4ecf358abcc64f79be2a30718ad8b
Jouni Seppänen <jouni.seppanen@reaktor.com>
2020-08-07 07:53:58 +0300

    Use certifi

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
+import certifi
 import io
 from urllib import request
 
 def test_fetching():
     url = 'https://matplotlib.org/1.5.0/_static/logo2.png'
-    with request.urlopen(url) as response:
+    with request.urlopen(url, cafile=certifi.where()) as response:
         response = io.BytesIO(response.read())
         assert len(response.getbuffer()) == 19496
